9f9867bae39d56e3e4670f8f3cc82161eace0e1a
David Ryan Delli-Gatti <Gatti@davids-mbp.attlocal.net>
2016-05-31 21:11:23 -0400

    it_works

---
+++ popup.js
@@ -0,0 +1,3 @@
+document.body.innerHTML = document.body.innerHTML.replace(/Charleston/gi, "New Ohio");
+//console.log("Testing");
+//alert (document.getElementsByClassName("thumbnail")[0].getAttribute("href"));
